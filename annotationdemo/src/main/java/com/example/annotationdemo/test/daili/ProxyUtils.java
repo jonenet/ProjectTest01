76a196c677efe9bc8e672df49e0e200b4c0a299b
ex-zhoulai <jonenet0619@gmail.com>
2018-05-04 15:54:24 +0800

    侧拉删除自定义控件

---
+++ annotationdemo/src/main/java/com/example/annotationdemo/test/daili/ProxyUtils.java
@@ -0,0 +1,39 @@
+package com.example.annotationdemo.test.daili;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import sun.misc.ProxyGenerator;
+
+/**
+ * Created by ex-zhoulai on 2018/5/4.
+ *
+ */
+
+public class ProxyUtils {
+
+    public static void generateClassFile(Class clazz, String proxyName) {
+
+        //根据类信息和提供的代理类名称，生成字节码
+        byte[] classFile = ProxyGenerator.generateProxyClass(proxyName, clazz.getInterfaces());
+        String paths = clazz.getResource(".").getPath();
+        System.out.println(paths);
+        FileOutputStream out = null;
+
+        try {
+            //保留到硬盘中
+            out = new FileOutputStream(paths + proxyName + ".class");
+            out.write(classFile);
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                assert out != null;
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- dialogapp/src/main/java/com/example/dragger/dialogapp/MainActivity.java
+++ dialogapp/src/main/java/com/example/dragger/dialogapp/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, NextActivity.class).putExtra("text", "123456789"));
+//        startActivity(new Intent(this, NextActivity.class).putExtra("text", "123456789"));
     }
 }
