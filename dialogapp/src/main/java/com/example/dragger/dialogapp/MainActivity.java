5ca615d0b74ba6542877e43d51d0eda97884ec73
ex-zhoulai <JONENET0619lai>
2018-05-21 18:54:17 +0800

    删除多余

--- dialogapp/src/main/java/com/example/dragger/dialogapp/MainActivity.java
+++ dialogapp/src/main/java/com/example/dragger/dialogapp/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
 
-    private MyDialog myDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- dialogapp/src/main/java/com/example/dragger/dialogapp/NextActivity.java
+++ dialogapp/src/main/java/com/example/dragger/dialogapp/NextActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.baselib.impl.AutowiredServiceImpl;
-import com.flipboard.bottomsheet.BottomSheetLayout;
 
 /**
  * Created by ex-zhoulai on 2018/4/28.
@@ -19,21 +18,12 @@ public class NextActivity extends AppCompatActivity {
 //
 //    @TestAnnoAware
 //    public String name;
-    protected BottomSheetLayout bottomSheetLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
         AutowiredServiceImpl.create().inject(this);
-
-        bottomSheetLayout = (BottomSheetLayout) findViewById(R.id.bottomsheet);
-        findViewById(R.id.bottomsheet_fragment_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new MyFragment().show(getSupportFragmentManager(), R.id.bottomsheet);
-            }
-        });
 //        ((TextView) findViewById(R.id.tv_text)).setText(text);
     }
 }
