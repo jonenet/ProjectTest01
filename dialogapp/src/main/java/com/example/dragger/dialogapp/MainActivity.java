506c401860e968c80912d216e1ce9f5cffa4c0cc
jonenet <jonenet0619@gmail.com>
2018-10-21 16:59:56 +0800

    add T

--- dialogapp/src/main/java/com/example/dragger/dialogapp/MainActivity.java
+++ dialogapp/src/main/java/com/example/dragger/dialogapp/MainActivity.java
@@ -1,26 +1,87 @@
 package com.example.dragger.dialogapp;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
 
 
+    private List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        startActivity(new Intent(this, NextActivity.class).putExtra("text", "123456789"));
+        list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add("test  = " + i);
+        }
+        findViewById(R.id.bottomsheet_fragment_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                showPopWindow(v, list);
+                startActivity(new Intent(MainActivity.this, NextActivity.class));
+            }
+        });
 
-//        findViewById(R.id.view).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                myDialog = new MyDialog(MainActivity.this);
-//                myDialog.show();
-//            }
-//        });
-        startActivity(new Intent(this, NextActivity.class).putExtra("text", "123456789"));
+    }
 
+
+    public void showPopWindow(final View view, final List<String> list) {
+        PopupWindow popupWindow = new PopupWindow();
+        View contentView = LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_popup_content, null, false);
+        RecyclerView rvPopup = contentView.findViewById(R.id.rv_popup);
+        RecyclerView.Adapter<RecyclerView.ViewHolder> adapter = new RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                TextView tv = new TextView(parent.getContext());
+                tv.setHeight(view.getHeight());
+                tv.setWidth(view.getWidth());
+                tv.setGravity(Gravity.CENTER_VERTICAL);
+                tv.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                tv.setTextSize(20);
+                return new RecyclerView.ViewHolder(tv) {
+                };
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                ((TextView) holder.itemView).setText(list.get(position));
+            }
+
+            @Override
+            public int getItemCount() {
+                return list.size();
+            }
+
+        };
+        rvPopup.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false));
+        rvPopup.setAdapter(adapter);
+        popupWindow.setContentView(contentView);
+        popupWindow.setWidth(view.getMeasuredWidth() - 8);
+        popupWindow.setHeight(view.getMeasuredHeight() * 5);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setFocusable(true);
+        popupWindow.setTouchable(true);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));
+        popupWindow.showAsDropDown(view,4,0);
     }
 }
--- dialogapp/src/main/java/com/example/dragger/dialogapp/NextActivity.java
+++ dialogapp/src/main/java/com/example/dragger/dialogapp/NextActivity.java
@@ -1,17 +1,26 @@
 package com.example.dragger.dialogapp;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
-import com.example.baselib.impl.AutowiredServiceImpl;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 /**
  * Created by ex-zhoulai on 2018/4/28.
  */
 
 public class NextActivity extends AppCompatActivity {
+    private ImageView ivScan;
 
 //    @TestAnnoAware
 //    public String text;
@@ -19,11 +28,58 @@ public class NextActivity extends AppCompatActivity {
 //    @TestAnnoAware
 //    public String name;
 
+    @SuppressLint("ObjectAnimatorBinding")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-        AutowiredServiceImpl.create().inject(this);
+//        AutowiredServiceImpl.create().inject(this);
 //        ((TextView) findViewById(R.id.tv_text)).setText(text);
+        ivScan = findViewById(R.id.iv_scan);
+        findViewById(R.id.step_one).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                one(ivScan);
+            }
+        });
+        findViewById(R.id.step_two).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                two(ivScan);
+            }
+        });
+
+        findViewById(R.id.step_three).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+    }
+
+    private void two(ImageView ivScan) {
+        ObjectAnimator[] objectAnimators = new ObjectAnimator[]{
+                ObjectAnimator.ofFloat(ivScan, "translationX", 0, -450),
+                ObjectAnimator.ofFloat(ivScan, "translationY", 0, 550),
+                ObjectAnimator.ofFloat(ivScan, "scaleX", 1f, 0.4f),
+                ObjectAnimator.ofFloat(ivScan, "scaleY", 1f, 0.4f)
+        };
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setDuration(1000).playTogether(objectAnimators);
+        animatorSet.setInterpolator(new DecelerateInterpolator());
+        animatorSet.start();
+    }
+
+    private void one(ImageView ivScan) {
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(ivScan, "rotation", 0f, 360f);
+        objectAnimator.setRepeatMode(ObjectAnimator.RESTART);
+        objectAnimator.setRepeatCount(ObjectAnimator.INFINITE);
+        objectAnimator.setDuration(1000);
+        objectAnimator.setInterpolator(new LinearInterpolator());
+        objectAnimator.start();
     }
 }
